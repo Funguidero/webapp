ce795ca2de462c137441875dde3504b12df0c3c7
JC-Pardo-Medina <jcparmed@gmail.com>
2021-12-16 18:10:59 +0100

    hola, héctor

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from app import app
 import urllib.request
 from flask import Flask, flash, request, redirect, url_for, render_template
-from flask_dropzone import Dropzone
+#from flask_dropzone import Dropzone
 from werkzeug.utils import secure_filename
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
@@ -11,26 +11,27 @@ def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 	
 @app.route('/')
-def upload_form():
+def index():
 	return render_template('index.html')
 
 @app.route('/', methods=['POST'])
 def upload_image():
-	if 'files[]' not in request.files:
+	if 'file' not in request.files:
 		flash('No file part')
 		return redirect(request.url)
-	files = request.files.getlist('files[]')
-	file_names = []
-	for file in files:
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file_names.append(filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		#else:
-		#	flash('Allowed image types are -> png, jpg, jpeg, gif')
-		#	return redirect(request.url)
-
-	return render_template('index.html', filenames=file_names)
+	uploaded_file = request.files['file']
+	if uploaded_file.filename == '':
+		flash('No image selected for uploading')
+		return redirect(request.url)
+	if uploaded_file and allowed_file(uploaded_file.filename):
+		filename = secure_filename(uploaded_file.filename)
+		uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		#print('upload_image filename: ' + filename)
+		flash('Image successfully uploaded and displayed below')
+		return render_template('index.html', filename=filename)
+	else:
+		flash('Allowed image types are -> png, jpg, jpeg, gif')
+		return redirect(request.url)
 
 @app.route('/display/<filename>')
 def display_image(filename):
@@ -38,4 +39,4 @@ def display_image(filename):
 	return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
