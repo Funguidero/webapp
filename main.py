9f3719171b56adf69424a46713114a99d2e7a6d4
JC-Pardo-Medina <jcparmed@gmail.com>
2021-12-16 14:02:15 +0100

    versionb palante

--- main.py
+++ main.py
@@ -9,42 +9,33 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
+	
 @app.route('/')
 def upload_form():
 	return render_template('index.html')
 
-app.config["UPLOAD_FOLDER"] = "uploads"
-
-def modify_time_sort(file_name):
-    file_path = "uploads/{}".format(file_name)
-    file_stats = os.stat(file_path)
-    last_access_time = file_stats.st_atime
-    return last_access_time
-
 @app.route('/', methods=['POST'])
 def upload_image():
-	if 'file' not in request.files:
+	if 'files[]' not in request.files:
 		flash('No file part')
 		return redirect(request.url)
-	file = request.files['file']
-	if file.filename == '':
-		flash('No image selected for uploading')
-		return redirect(request.url)
-	if file and allowed_file(file.filename):
-		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		#print('upload_image filename: ' + filename)
-		flash('Image successfully uploaded and displayed below')
-		return render_template('index.html', filename=filename)
-	else:
-		flash('Allowed image types are -> png, jpg, jpeg, gif')
-		return redirect(request.url)
+	files = request.files.getlist('files[]')
+	file_names = []
+	for file in files:
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file_names.append(filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		#else:
+		#	flash('Allowed image types are -> png, jpg, jpeg, gif')
+		#	return redirect(request.url)
+
+	return render_template('index.html', filenames=file_names)
 
 @app.route('/display/<filename>')
 def display_image(filename):
 	#print('display_image filename: ' + filename)
-	return redirect(filename='static/uploads/' + filename, code=301)
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 if __name__ == "__main__":
     app.run()
